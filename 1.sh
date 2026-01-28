408c8271f6369ed8435117f15e5bb1fe98c8438c
KishoHiga <80713494+KishoHiga@users.noreply.github.com>
2026-01-28 22:56:00 +0300

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,48 @@
+Last login: Wed Jan 28 21:30:58 on ttys001
+ivanbrening@MacBook-Pro ~ % nano 1.sh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  UW PICO 5.09                       File: 1.sh                       Modified  
+
+#!/bin/bash 
+echo "Hello, mike!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+^G Get Help  ^O WriteOut  ^R Read File ^Y Prev Pg   ^K Cut Text  ^C Cur Pos   
+^X Exit      ^J Justify   ^W Where is  ^V Next Pg   ^U UnCut Text^T To Spell  
